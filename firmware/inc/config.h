0533ba5ef50c0090d3f1e6c0b090f9672602f1c6
alhaddad-m <ubuntu.msa@gmail.com>
2025-09-05 09:58:57 +0300

    Add config.h file

---
+++ firmware/inc/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define UART_TX_PIN (0)
+#define HARP_SYNC_RX_PIN (5)
+#define HARP_CORE_LED_PIN (25)
+
+#define HARP_DEVICE_ID (0)
+
+#define HW_VERSION_MAJOR (1)
+#define HW_VERSION_MINOR (0)
+#define HW_ASSEMBLY_VERSION (0)
+
+#define FW_VERSION_MAJOR (0)
+#define FW_VERSION_MINOR (0)
+#define FW_VERSION_PATCH (1)
+
+#define UNUSED_SERIAL_NUMBER (0)  // Deprecated in favor of R_UUID
+
+#endif // CONFIG_H
